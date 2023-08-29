bdfc98c140aba342fd25c6e3693fb8175e7f977d
turkertunay <75833839+turkertunay@users.noreply.github.com>
2023-08-29 19:31:45 +0300

    Add files via upload

---
+++ 015_for.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  <?php
+      $index =1;
+      for($i = 1; $i <= 5; $i++){
+          echo "$i <br>";
+          }
+      $numbers = array(2, 11, 17, 23, 35, 47);
+      for($i =0; $i < count($numbers); $i++){
+          echo "$numbers[$i] <br>";
+          }
+
+
+  ?>
+  </body>
+</html>
